99c5e152e15f55af14ac0d8db90c33b77d041e3e
Brayden <humortronic@gmail.com>
2024-12-01 11:57:48 +1030

    Added changes to code

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import pygame
 import math
 
 pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("RCS Thruster Simulation")
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    
